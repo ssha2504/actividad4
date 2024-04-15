fd6dda09d857425792068d30cacf078a71b32e59
ssha2504 <ssha2504@g.educaand.es>
2024-04-15 12:18:35 +0200

    randow anterior

--- EclipseGit/actividad4/src/actividad4/Profesor.java
+++ EclipseGit/actividad4/src/actividad4/Profesor.java
@@ -1,25 +1,15 @@
 package actividad4;
-import java.util.Random;
 
-public class Profesor {
-	private Random random;
-
-    public Profesor() {
-        random = new Random();
-    }
-
+class Profesor {
     public void ponerNotas(Alumno alumno) {
-        alumno.getAsignatura1().setCalificacion(generarCalificacionAleatoria());
-        alumno.getAsignatura2().setCalificacion(generarCalificacionAleatoria());
-        alumno.getAsignatura3().setCalificacion(generarCalificacionAleatoria());
+        alumno.getAsignatura1().setCalificacion(Math.random() * 10);
+        alumno.getAsignatura2().setCalificacion(Math.random() * 10);
+        alumno.getAsignatura3().setCalificacion(Math.random() * 10);
     }
 
     public double calcularMedia(Alumno alumno) {
         return (alumno.getAsignatura1().getCalificacion() + alumno.getAsignatura2().getCalificacion() +
                 alumno.getAsignatura3().getCalificacion()) / 3.0;
     }
-
-    private double generarCalificacionAleatoria() {
-        return random.nextDouble() * 10; // calificación aleatoria entre 0 y 10
-    }
 }
+
